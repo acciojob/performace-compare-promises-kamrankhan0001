749324871775f28c57f95d194398949803c88010
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-19 23:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchData() {
+  // create an array of promises that fetch data from each API url
+  const promises = apiUrls.map(url => fetch(url));
+
+  // measure the time taken for Promise.all to complete the API calls
+  const startAll = performance.now();
+  Promise.all(promises)
+    .then(responses => {
+      const endAll = performance.now();
+      const timeAll = endAll - startAll;
+      // display the time taken in the table with id "output-all"
+      document.getElementById("output-all").textContent = `${timeAll.toFixed(2)} ms`;
+    })
+    .catch(error => console.error(error));
+
+  // measure the time taken for Promise.any to complete the API calls
+  const startAny = performance.now();
+  Promise.any(promises)
+    .then(response => {
+      const endAny = performance.now();
+      const timeAny = endAny - startAny;
+      // display the time taken in the table with id "output-any"
+      document.getElementById("output-any").textContent = `${timeAny.toFixed(2)} ms`;
+    })
+    .catch(error => console.error(error));
+}
+
+// call the fetchData function on page load
+window.addEventListener("load", fetchData);
